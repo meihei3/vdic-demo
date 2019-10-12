41aabacb98a8c53c8c4a85e34dce2d5f10a1a78e
yameholo <ame.aroid.xz5@gmail.com>
2019-10-13 04:39:13 +0900

    Update parser

--- tools/data_parser.py
+++ tools/data_parser.py
@@ -6,12 +6,43 @@ __file_name = "V分類.txt"
 DATA_PATH = __data_dir + '/' + __file_name
 
 
-class RegexPattern:
-    name = "🌟\s(.+)\n"
-    looks = "見た目：[　|\s]?(.+)\n"
-    fact = "活動内容：[　|\s]?(.+)\n"
-    settings = "設定など：[　|\s]?(.+)\n"
-    other = "その他：[　|\s]?(.*)"
+class Data:
+    __ptn_name = regex.compile(r"🌟\s?(.+)\n")
+    __ptn_looks = regex.compile(r"見た目：[　|\s]?(.*)\n")
+    __ptn_contents = regex.compile(r"活動内容：[　|\s]?(.*)\n")
+    __ptn_settings = regex.compile(r"設定など：[　|\s]?(.*)\n")
+    __ptn_other = regex.compile(r"その他：[　|\s]?(.*)")
+
+    def __init__(self, raw: str):
+        self.name: str = None
+        self.looks: str = None
+        self.contents: str = None
+        self.settings: str = None
+        self.other: str = None
+
+        self.__parse(raw)
+
+
+    def __parse(self, raw: str):
+        for key, ptn in self.key_ptn().items():
+            g = ptn.search(raw)
+            if g and len(g) >= 2:
+                value = g[1] if key == "name" else g[1].split('、')
+                setattr(self, key, value)
+            else:
+                assert g, "%s not match" % key
+                assert len(g) >= 2, "%s not get groups" % key
+
+
+    def key_ptn(self):
+        return {
+            "name": self.__ptn_name,
+            "looks": self.__ptn_looks,
+            "contents": self.__ptn_contents,
+            "settings": self.__ptn_settings,
+            "other": self.__ptn_other,
+        }
+
 
 
 def get_raw(file_path: str = DATA_PATH):
@@ -22,8 +53,13 @@ def get_raw(file_path: str = DATA_PATH):
 
 def parse_data(raw_data: str):
     for x in raw_data.split("\n\n"):
-        print("-"*30)
-        print(x)
+        print("-"*20)
+        data = Data(x)
+        print("name:", data.name)
+        print("looks:", data.looks)
+        print("contents:", data.contents)
+        print("settings:", data.settings)
+        print("other:", data.other)
 
 
 if __name__ == "__main__":
